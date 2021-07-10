ec2631775535de9ef5c2d74dfaeeb17860e77342
AbdullahM0hamed <25087116+AbdullahM0hamed@users.noreply.github.com>
2021-07-10 15:05:18 +0100

    SharedPreferences, ayah count IX

--- app/src/main/kotlin/com/mushaf/android/data/PreferenceHelper.kt
+++ app/src/main/kotlin/com/mushaf/android/data/PreferenceHelper.kt
@@ -23,14 +23,16 @@ object PreferenceHelper {
     fun getCurrentMushaf(): Mushaf? {
         val prefs = getPreferences()
         val currentMushaf = prefs.getString(CURRENT_MUSHAF_KEY, "")
+        val locationKey = currentMushaf + LOCATION_SUFFIX
+        val dbKey = currentMushaf + DB_SUFFIX
 
         return if (currentMushaf != "") {
             Mushaf(
                 currentMushaf.split("_").get(0),
                 currentMushaf.split("_").get(1) as Int,
-                prefs.getString(currentMushaf + LOCATION_SUFFIX, ""),
+                prefs.getString(locationKey, ""),
                 prefs.getBoolean(currentMushaf + DOWNLOADED_SUFFIX, false),
-                prefs.getString(currentMushaf + DB_SUFFIX, "")
+                prefs.getString(dbKey, "")
             )
         } else {
             null
