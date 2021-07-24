17313805e3d2d3f707d65b253b700dc7cd7c29fd
AbdullahM0hamed <25087116+AbdullahM0hamed@users.noreply.github.com>
2021-07-24 20:03:27 +0100

    Fic CurlView bugs XVIII

--- app/src/main/kotlin/com/mushaf/android/ui/page/curl/SinglePageRender.kt
+++ app/src/main/kotlin/com/mushaf/android/ui/page/curl/SinglePageRender.kt
@@ -78,7 +78,7 @@ class SinglePageRender(
 
                 if (state == PageFlipState.END_WITH_FORWARD) {
                     pageNo--
-                    pageFlip.getFirstPage().setSecondTexture(getImageFromZip(pageNo))
+                    pageFlip.getFirstPage().setFirstTextureWithSecond()
                 }
 
                 drawCommand = DRAW_FULL_PAGE
