fda0c8761752bcbfde7e1e59567812254c3815b3
AbdullahM0hamed <25087116+AbdullahM0hamed@users.noreply.github.com>
2021-07-24 14:44:13 +0100

    Fix CurlView bugs IX

--- app/src/main/kotlin/com/mushaf/android/ui/page/curl/SinglePageRender.kt
+++ app/src/main/kotlin/com/mushaf/android/ui/page/curl/SinglePageRender.kt
@@ -100,7 +100,7 @@ class SinglePageRender(
         val pageCount = mushaf!!.getPageCount()
 
         if (pageNo < pageCount) {
-            pageFlip.getFirstPage().setSecondTextureWithFirst()
+            pageFlip.getFirstPage().setFirstTextureWithSecond()
             return true
         }
 
