f9091e5c91f68a9267500a4a67b6cb8cabe34dfc
AbdullahM0hamed <25087116+AbdullahM0hamed@users.noreply.github.com>
2022-04-06 18:57:25 +0100

    Show loading dialog IV

--- app/src/main/kotlin/com/mushaf/android/Reducers.kt
+++ app/src/main/kotlin/com/mushaf/android/Reducers.kt
@@ -12,7 +12,7 @@ fun riwaayahStateReducer(state: RiwaayahState, action: Any): RiwaayahState {
     var currentState = state
 
     if (action is DownloadRiwaayah) {
-        currentState.copy(action.riwaayah, action.riwaayah_id)
+        currentState.copy(action.riwaayah, action.riwaayahId)
     }
 
     return currentState
